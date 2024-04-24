257a54b60454c51b0803d4f626e48f117e472e09
Cody Tedrick <cody.tedrick@cavrn.us>
2024-04-24 15:52:05 -0700

    Add chat menu to car scene. Add new enum options for car color

---
+++ Assets/Cavrnus Content/Scripts/Car/SyncEnumTextureStrings.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CavrnusSdk.API;
+using Collab.Proxy.Prop.StringProp;
+using UnityEngine;
+
+namespace Cavrnus_Content.Scripts.Car
+{
+    public class SyncEnumTextureStrings : MonoBehaviour
+    {
+        [Serializable]
+        public class TextureMapper
+        {
+            public string TextureName;
+            public Texture TextureValue;
+        }
+        
+        [Header("Cav Properties")]
+        [SerializeField] private string containerName;
+        [SerializeField] private string propertyName;
+        [SerializeField] private string displayName;
+        [SerializeField] private string description;
+
+        [Header("Texture & Material")]
+        [SerializeField] private List<TextureMapper> textureMap;
+        [SerializeField] private Material targetMaterial;
+        
+        private IDisposable disp;
+
+        private void Start()
+        {
+            CavrnusFunctionLibrary.AwaitAnySpaceConnection(spaceConn => {
+                var enumOptions = new List<StringEditingEnumerationOption>();
+                textureMap.ForEach(tm => enumOptions.Add(new StringEditingEnumerationOption {
+                    DisplayText = tm.TextureName,
+                    EnumValue = tm.TextureName
+                }));
+
+                spaceConn.DefineStringPropertyDefinition(containerName, propertyName, displayName, description, false, enumOptions);
+
+                disp = spaceConn.BindStringPropertyValue(containerName, propertyName, serverTextureName => {
+                    var newTexture = textureMap.FirstOrDefault(tm => tm.TextureName == serverTextureName);
+                    if (newTexture != null) 
+                        targetMaterial.mainTexture = newTexture.TextureValue;
+                    else {
+                        Debug.LogWarning($"TextureValue is invalid! Trying to update with server value of {serverTextureName}");
+                    }
+                });
+            });
+        }
+        
+        private void OnDestroy()
+        {
+            disp?.Dispose();
+        }
+    }
+}
