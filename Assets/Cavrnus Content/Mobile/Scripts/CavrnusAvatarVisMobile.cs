25deb1845afae03e87ab06b6715596ce10f0b00f
Cody Tedrick <cody.tedrick@cavrn.us>
2024-09-05 14:10:39 -0700

    Hiding user on mobile devices

---
+++ Assets/Cavrnus Content/Mobile/Scripts/CavrnusAvatarVisMobile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using CavrnusSdk.API;
+using UnityEngine;
+
+namespace Cavrnus_Content.Mobile.Scripts
+{
+    public class CavrnusAvatarVisMobile : MonoBehaviour
+    {
+        private void Start()
+        {
+            CavrnusFunctionLibrary.AwaitAnySpaceConnection(sc => {
+                sc.AwaitLocalUser(user => {
+                    StartCoroutine(CheckPlatformWithDelay(user, 3f)); // Just to be extra certain...
+                });
+            });
+        }
+
+        private IEnumerator CheckPlatformWithDelay(CavrnusUser user, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+
+            if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+            {
+                Debug.Log("Running on a mobile device");
+                user.SpaceConnection.BeginTransientBoolPropertyUpdate(user.ContainerId, "AvatarVis", false);
+            }
+            else
+            {
+                Debug.Log("Not running on a mobile device");
+            }
+        }
+    }
+}
